1cf0eb8d2c336a7f681bee1e20d065ed5e716981
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-14 15:37:52 +0900

    Remove wrap.js

--- wrap.js
+++
@@ -1,9 +0,0 @@
-import SuperIterable from "./SuperIterable";
-export default function wrap(xs) {
-    if (xs instanceof Function) {
-        return new SuperIterable({
-            [Symbol.iterator]: xs
-        });
-    }
-    return new SuperIterable(xs);
-}

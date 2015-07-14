71e2f7ed3a0c6b68c862bd6a7b190987eba382cc
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-14 11:43:30 +0900

    Add test

---
+++ test/test.ts
@@ -0,0 +1,11 @@
+import {assert} from "chai";
+
+describe("SuperIterable", () => {
+  describe("#map", () => {
+    it("maps values", () => {
+      const it = _([1,2,3]);
+      const mapped = it.map(x => x * 2).toArray();
+      assert.deepEqual(mapped, [2,4,6]);
+    });
+  });
+});
